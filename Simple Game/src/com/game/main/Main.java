7449d42519e82698fd8d75c97b0a000330519e61
Tyler <t.eldred4646@gmail.com>
2016-07-03 10:18:17 -0600

    added content to "Simple Game"

--- Simple Game/src/com/game/main/Main.java
+++ Simple Game/src/com/game/main/Main.java
@@ -1,10 +1,34 @@
 package com.game.main;
 
-public class Main {
+import java.awt.*;
+
+import javax.swing.*;
+
+public class Main extends JComponent{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		JFrame window = new JFrame("New Game");
+		Main game = new Main();
+		window.add(game);
+		window.pack();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+	}
+	public Dimension getPreferredSize() {
+		return new Dimension(800, 600);
+	}
+	
+	protected void paintComponent(Graphics g) {
+		g.setColor(Color.black);
+		g.fillRect(0, 0, 800, 600);
+		
+		g.setColor(Color.red);
+		g.fillOval(375, 275, 50, 50);
+		
+		g.setFont(new Font(Font.DIALOG, Font.BOLD, 18));
+		g.drawString("This is a red circle", 315, 400);
 	}
 
 }
